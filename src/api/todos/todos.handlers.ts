93c05068b8dee2849af3ef9dd2cb9017117e2455
Jay M Gonzalez <jaimungon@gmail.com>
2022-09-22 13:31:05 +0200

    add create one route

--- src/api/todos/todos.handlers.ts
+++ src/api/todos/todos.handlers.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express'
-import { Todos, TodoWithId } from './todos.model'
+import { InsertOneResult } from 'mongodb'
+import { Todo, Todos, TodoWithId } from './todos.model'
 
 export async function findAll(
   req: Request,
@@ -9,6 +10,20 @@ export async function findAll(
   try {
     const result = Todos.find()
     res.json(await result.toArray())
+  } catch (err) {
+    next(err)
+  }
+}
+
+export async function createOne(
+  req: Request<{}, InsertOneResult<Todo>, Todo>,
+  res: Response<InsertOneResult>,
+  next: NextFunction
+) {
+  try {
+    const validateResult = await Todo.parse(req.body)
+    const insertResult = await Todos.insertOne(validateResult)
+    res.json(insertResult)
   } catch (err) {
     next(err)
   }
