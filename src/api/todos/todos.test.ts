0b48bc7049bcac3645398d7f450bfae29493511f
Jay M Gonzalez <jaimungon@gmail.com>
2022-09-23 12:25:29 +0200

    define tests for getOne route

--- src/api/todos/todos.test.ts
+++ src/api/todos/todos.test.ts
@@ -37,6 +37,7 @@ describe('POST /api/v1/todos', () => {
       }))
 })
 
+let id: string = ''
 describe('POST /api/v1/todos', () => {
   it('respond with an inserted object', async () =>
     request(app)
@@ -50,9 +51,43 @@ describe('POST /api/v1/todos', () => {
       .expect(201)
       .then((response) => {
         expect(response.body).toHaveProperty('_id')
+        id = response.body._id
         expect(response.body).toHaveProperty('content')
         expect(response.body.content).toBe('Learn TypeScript')
         expect(response.body).toHaveProperty('done')
         expect(response.body.done).toBe(true)
       }))
+})
+
+describe('GET /api/v1/todos/:id', () => {
+  it('respond with a single todo', async () =>
+    request(app)
+      .get(`/api/v1/todos/${id}`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toHaveProperty('_id')
+        expect(response.body._id).toBe(id)
+        expect(response.body).toHaveProperty('content')
+        expect(response.body.content).toBe('Learn TypeScript')
+        expect(response.body).toHaveProperty('done')
+        expect(response.body.done).toBe(true)
+      }))
+
+  it('respond with an invalid object ID error', (done) => {
+    request(app)
+      .get('/api/v1/todos/invalid-id')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /html/)
+      .expect(422, done)
+  })
+
+  it('respond with a not found error', (done) => {
+    request(app)
+      .get('/api/v1/todos/632d617419847fa1f664d174')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /html/)
+      .expect(404, done)
+  })
 })
