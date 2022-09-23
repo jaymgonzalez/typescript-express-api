f0296208928c49449c3e360ef9b5631a3f1b199c
Jay M Gonzalez <jaimungon@gmail.com>
2022-09-23 17:28:20 +0200

    update db info

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,9 @@
 import { MongoClient } from 'mongodb'
 
-const { MONGO_URI = 'mongodb://localhost:27017/todo-api' } = process.env
+const { MONGO_HOST, MONGO_USERNAME, MONGO_PASSWORD, MONGO_PORT, MONGO_DBNAME } =
+  process.env
+
+const MONGO_URI = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DBNAME}?authSource=admin`
 
 export const client = new MongoClient(MONGO_URI)
 export const db = client.db()
